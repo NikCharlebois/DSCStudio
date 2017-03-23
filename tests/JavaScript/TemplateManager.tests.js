e5fefc99bfdca1bcf6445bbf227753be05475896
Brian Farnhill <brfarn@microsoft.com>
2017-03-23 16:37:21 +1100

    First couple of actually useful tests

--- tests/JavaScript/TemplateManager.tests.js
+++ tests/JavaScript/TemplateManager.tests.js
@@ -1,9 +1,33 @@
 var assert = require("assert");
-var TemplateManager = require("../../src/scripts/TemplateManager");
-// commenting this out until I can figure out the compiler for ES6 tests to run
+var sinon = require("sinon");
+var TemplateManager = require("../../src/scripts/TemplateManager").default;
 
-describe("FakeTest", function(){
-  it("Should have some tests written soon!", function() {
-    assert(true === true, "Dummy test");
+global.DynamicTemplate = undefined;
+
+describe("TemplateManager - Init method", function() {
+  it("Should alert when appropriate APIs are not supported", function() {
+    var alert = sinon.spy();
+    global.alert = alert;
+    global.window = {};
+
+    var returnVal = TemplateManager.Init();
+
+    assert(returnVal === false, "Init method did not return false when the initialisation failed");
+    assert(alert.calledOnce, "Alert was not called during the method");
+  });
+
+  it ("Should not return a value when appropriate APIs are supported", function () {
+    var alert = sinon.spy();
+    global.alert = alert;
+    global.window = {
+      File: {},
+      FileReader: {},
+      FileList: {},
+      Blob: {}
+    };
+
+    var returnVal = TemplateManager.Init();
+    assert(returnVal === undefined, "Init method should equal 'undefined' when it succeeds");
+    assert(alert.notCalled, "Alert was called during the method");
   });
 });
